2700784d62249790d663bddd6e48e7ac24681bca
Oliver <rtw0913@gmail.com>
2025-01-07 23:36:56 +0800

    建立專案

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+
+  <!-- web layout -->
+   
+  <!-- 側邊欄 -->
+  <!-- main -->
+
+
+
+
+  <RouterView />
+</template>
