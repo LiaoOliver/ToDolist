f19b3b52aa70c4abc31275e059143e0d968d6638
Oliver Liao <rtw0913@gmail.com>
2025-01-08 22:20:14 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,10 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === 'production' ? '/<repository-name>/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/todolist/' : '/',
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        entryFileNames: `assets/[name].[hash].js`,
-        chunkFileNames: `assets/[name].[hash].js`,
-        assetFileNames: `assets/[name].[hash].[ext]`
-      }
-    }
+    outDir: 'dist'
   }
 })
