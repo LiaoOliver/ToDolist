a6a801098d72a6b9377dca6cbddb5dd55e99baf1
Oliver Liao <rtw0913@gmail.com>
2025-01-08 22:01:14 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
-module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/todolist/' // 替換為你的倉庫名稱
-      : '/'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [vue()],
+  base: process.env.NODE_ENV === 'production' ? '/todolist/' : '/',
+  build: {
+    outDir: 'dist'
   }
+})
